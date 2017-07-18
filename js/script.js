3194ab285f03c3e88bbe7e25281b97df0a0543c6
RobotHabanera <robothabaneradance@gmail.com>
2017-07-18 22:56:50 +0200

    new

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@
 
 
                       $(".c-main-menu").animate({"left": "0px"}, 350);
-                      $("body").animate({ "left": "200px"}, 350, function() { $("body").addClass("fade"); });  
+                      $("body").animate({ "left": "200px"}, 350);
 
 
                     }); 
@@ -94,11 +94,9 @@
                 scrollTop: $($.attr(this, "href")).offset().top -60
             }, 1000, function() { 
                 
-                $(".c-main-menu").animate({"left": "-200px"}, 
-                { duration: 200, queue: false });
+                $(".c-main-menu").animate({"left": "-200px"}, 200);
                                
-                $("html,body").animate({"left": "0px"},
-                {duration:200, quee: false});
+                $("html,body").animate({"left": "0px"}, 200);
       
         });
             
