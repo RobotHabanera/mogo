b7a57b85a642e2eb6707dd37412c42b51412d9d9
RobotHabanera <robothabaneradance@gmail.com>
2017-07-20 21:09:47 +0200

    deee

--- js/script.js
+++ js/script.js
@@ -581,7 +581,7 @@ jQuery(function($){
                         scrollTop: $target.position().top - 80
                     }, 1200, 'swing', function (e) {
                         e.preventDefault();
-                        window.location.hash = $target;
+                        window.location.hash = target;
                         $(document).on("scroll", onScroll);
                     }); 
 
