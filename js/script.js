2c64c11795dd4c807e996cb7ed24193a6867408e
RobotHabanera <robothabaneradance@gmail.com>
2017-07-22 08:58:42 +0200

    egfds

--- js/script.js
+++ js/script.js
@@ -576,7 +576,7 @@ jQuery(function($){
                     $(this).prepend('<div class="object1"></div>');
      
              var $target = $(this.hash), target = this.hash;
-             var targetOffset = $target.offset().top - 70;
+             var targetOffset = target.offset().top - 70;
                         menu = target;
                     $target = $(target);
                     $('html, body').stop().animate({
