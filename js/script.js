58aab7761ffd993ac3b66916166991a13689e6d8
RobotHabanera <robothabaneradance@gmail.com>
2017-07-22 18:59:54 +0200

    fdsr

--- js/script.js
+++ js/script.js
@@ -976,23 +976,6 @@ $('.open-map').on("click", function() {
 
 
 
-/* Wywołanie pluginu nice scroll z custumowymi ustawieniami scrollbara */
-   
-
-    $('html,body').niceScroll({
-        
-        cursorcolor: "#fff",
-        cursorwidth: "8px",
-        cursoropacitymax: .5,
-        cursorborder: "1px solid darkgrey",
-        background: "",
-        horizrailenabled:false
-        
-        
-        
-    });
-    
-
 
 
 
